7de517250119df99c98acac10cea7629be90c668
anhelov-y <yuriy.anhelov@gmail.com>
2026-06-08 16:00:47 +0300

    UPD_fix_6

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,15 @@ import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 
 export default defineConfig({
-  // Залізобетонний шлях для репозиторію
   base: '/goit-advancedjs-hw-02/',
 
   root: 'src',
   build: {
     rollupOptions: {
       input: {
-        main: 'src/index.html',
-        timer: 'src/1-timer.html',
-        snackbar: 'src/2-snackbar.html',
+        main: './index.html',
+        timer: './1-timer.html',
+        snackbar: './2-snackbar.html',
       },
     },
     outDir: '../dist',
