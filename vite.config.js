4ddf78ef573eae75daf8f45a34b2bb7eab52d718
anhelov-y <yuriy.anhelov@gmail.com>
2026-06-08 13:15:14 +0300

    UPD_vite

--- vite.config.js
+++ vite.config.js
@@ -1,48 +1,20 @@
 import { defineConfig } from 'vite';
-import { glob } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
-import SortCss from 'postcss-sort-media-queries';
 
-export default defineConfig(({ command }) => {
-  return {
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
-    },
-    root: 'src',
-    build: {
-      sourcemap: true,
-      rollupOptions: {
-        input: glob.sync('./src/*.html'),
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-          entryFileNames: chunkInfo => {
-            if (chunkInfo.name === 'commonHelpers') {
-              return 'commonHelpers.js';
-            }
-            return '[name].js';
-          },
-          assetFileNames: assetInfo => {
-            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
-              return '[name].[ext]';
-            }
-            return 'assets/[name]-[hash][extname]';
-          },
-        },
+export default defineConfig({
+  base: '/goit-advancedjs-hw-02/',
+
+  root: 'src',
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        timer: './1-timer.html',
+        snackbar: './2-snackbar.html',
       },
-      outDir: '../dist',
-      emptyOutDir: true,
     },
-    plugins: [
-      injectHTML(),
-      FullReload(['./src/**/**.html']),
-      SortCss({
-        sort: 'mobile-first',
-      }),
-    ],
-  };
+    outDir: '../dist',
+  },
+  plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
 });
