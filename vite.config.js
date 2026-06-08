39c7bc82363808935d39c7aa65d99fa205b5e10a
anhelov-y <yuriy.anhelov@gmail.com>
2026-06-08 16:19:26 +0300

    UPD_FIX_3

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,20 @@ import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 
 export default defineConfig({
-  base: '',
+  root: 'src',
+
+  base: process.env.NODE_ENV === 'production' ? './' : '/',
 
   build: {
     rollupOptions: {
       input: {
-        main: 'src/index.html',
-        timer: 'src/1-timer.html',
-        snackbar: 'src/2-snackbar.html',
+        main: './index.html',
+        timer: './1-timer.html',
+        snackbar: './2-snackbar.html',
       },
     },
-    outDir: 'dist',
+    outDir: '../dist',
     emptyOutDir: true,
   },
-  plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+  plugins: [injectHTML(), FullReload(['./**/*.html'])],
 });
