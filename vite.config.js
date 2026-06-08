d3a78a77b19fa1a0544a5a0da23e9b29877210a1
anhelov-y <yuriy.anhelov@gmail.com>
2026-06-08 16:43:14 +0300

    UPD_fix5

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,24 @@
-export default defineConfig(({ command }) => ({
-  root: 'src',
-  base: command === 'serve' ? '/' : '/goit-advancedjs-hw-02/',
-  
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'src/index.html'),
-        timer: resolve(__dirname, 'src/1-timer.html'),
-        snackbar: resolve(__dirname, 'src/2-snackbar.html'),
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
+
+export default defineConfig(({ command }) => {
+  return {
+    root: 'src',
+    base: command === 'serve' ? '/' : '/goit-advancedjs-hw-02/',
+    
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'src/index.html'),
+          timer: resolve(__dirname, 'src/1-timer.html'),
+          snackbar: resolve(__dirname, 'src/2-snackbar.html'),
+        },
       },
+      outDir: '../dist',
+      emptyOutDir: true,
     },
-    outDir: '../dist',
-    emptyOutDir: true,
-  },
-  plugins: [injectHTML(), FullReload(['./**/*.html'])],
-}));
+    plugins: [injectHTML(), FullReload(['./**/*.html'])],
+  };
+});
